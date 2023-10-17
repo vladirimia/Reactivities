958bd84aa81f81c13698e9374c42c29c9a14158c
irimia <irimia@HCBN3T3>
2023-10-17 16:51:24 +0300

    Add project files.

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+
+public class ActivitiesController : BaseApiController
+{
+    private readonly DataContext _context;
+
+    public ActivitiesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetActivities()
+    {
+        return Ok(await _context.Activities.ToListAsync());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetActivity(Guid id)
+    {
+        return Ok(await _context.Activities.FindAsync(id));
+    }
+}
---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class BaseApiController : ControllerBase
+{
+}
